891f3126bbac43a0dd3c7972900d31f1b5f3e361
carlos bazan <pacoyx@gmail.com>
2019-05-13 17:16:33 -0500

    agregando clase personal

---
+++ Persona.cs
@@ -0,0 +1,9 @@
+public class Persona{
+
+public string Mensaje(){
+
+    Console.WriteLine("Datos de la persona:");
+}
+
+
+}
--- Program.cs
+++ Program.cs
@@ -10,14 +10,21 @@ namespace holam
 			Console.WriteLine("Linea de codigo antes de subirlo al gitHub");
 
 
-						Console.WriteLine("Linea de cambio para probar funcionalidad");
+            Console.WriteLine("Linea de cambio para probar funcionalidad");
 
 
-						Console.WriteLine("Linea de cambio en el estado");
-						Console.WriteLine("Linea de cambio en las variables globales");
+            Console.WriteLine("Linea de cambio en el estado");
+            Console.WriteLine("Linea de cambio en las variables globales");
 
-						Console.WriteLine("Linea de cambio en las variables staticas");
-						Console.WriteLine("Linea de cambio en los sub modulos principales");
+            Console.WriteLine("Linea de cambio en las variables staticas");
+            Console.WriteLine("Linea de cambio en los sub modulos principales");
+
+
+            //personal
+            Personal clsper = new Personal();
+            clsper.Mensaje();
+
+
         }
     }
 }
