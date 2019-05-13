b405f1b5167b06c86a6cff657acfc9f6d73819fc
carlos bazan <pacoyx@gmail.com>
2019-05-13 14:43:44 -0500

    SUbiendo a la nube

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace holam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+			Console.WriteLine("Linea de codigo antes de subirlo al gitHub");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Hola mundo c# console
